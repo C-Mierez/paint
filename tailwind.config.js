b4c40c772c2a7d4b9f2905daaa7467f7efbed4b8
C-Mierez <carlos.mierez20@gmail.com>
2024-10-24 03:48:42 -0300

    🌈 style: Extend Tailwind with custom colors and border

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: {
+                    100: "#E2E7EB",
+                    200: "#BFBFBF",
+                    500: "#848484",
+                    800: "#303030",
+                },
+                primary: {
+                    500: "#020180",
+                },
+            },
+            borderWidth: {
+                win: "2px",
+            },
+        },
     },
     plugins: [],
 };
